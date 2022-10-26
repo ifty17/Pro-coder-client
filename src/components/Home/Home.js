3f3e58dca629649ea9bfcefe4b76a1fed7f58d60
ifty17 <iftyrahatkabir@gmail.com>
2022-10-26 16:00:34 +0600

    added landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import homeimg from '../../assects/homeimg.png'
+import Header from '../Header/Header';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is home</h2>
+      <div>
+        <Header></Header>
+        <div className="flex items-center my-5 justify-center flex-col sm:flex-col md:flex-row">
+          <div className=" md:w-[50%]">
+            <h1 className="text-5xl text-left py-5">
+              Learn Effectively that will take you towards your goal
+            </h1>
+            <button
+              type="button"
+              class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
+            >
+              Download course documentation
+            </button>
+          </div>
+          <div className="w-max">
+            <img src={homeimg} alt="" />
+          </div>
         </div>
+      </div>
     );
 };
 
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -2,17 +2,19 @@ import { Container } from 'postcss';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
+import Home from '../components/Home/Home';
 import LeftSideCategory from '../components/LeftSideCategory/LeftSideCategory';
 
 const Main = () => {
     return (
-      <div>
+      <div className='mx-5'>
         <Header></Header>
-        <div>
-          <div className="grid grid-cols-4">
+        
+        <div className="flex">
+          <div className="p-5 grid grid-cols-2 border">
             <LeftSideCategory></LeftSideCategory>
           </div>
-          <div className="grid grid-cols-8">
+          <div className="grid grid-cols-10 ">
             <Outlet></Outlet>
           </div>
         </div>

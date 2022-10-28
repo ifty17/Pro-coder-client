4d1c055321d87b9d88a20a94c4e88e186e1468a1
ifty17 <iftyrahatkabir@gmail.com>
2022-10-28 19:16:45 +0600

    edited home content

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,7 +18,7 @@ const Home = () => {
         <div className="flex items-center mt-3 justify-center flex-col sm:flex-col md:flex-row">
           <div className=" w-[90%] md:w-[50%]">
             <h1 className="text-5xl text-left py-5">
-              Your dream to become a full stack developer is on step away!
+              Your dream to become a full stack developer is one step away!
             </h1>
             <p className='py-3'>Passion + Dedication + Hard Word = Success!</p>
             <div>

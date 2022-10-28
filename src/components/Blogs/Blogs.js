c6e0e0fb9f736565cfbf23c24f3dc653f9a5c75d
ifty17 <iftyrahatkabir@gmail.com>
2022-10-28 19:11:54 +0600

    added home content

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,9 +2,89 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>this is blogs</h1>
+      <div>
+        <div
+          tabIndex={0}
+          className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            what is cors?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Cross-Origin Resource Sharing (CORS) is an HTTP-header based
+              mechanism that allows a server to indicate any origins (domain,
+              scheme, or port) other than its own from which a browser should
+              permit loading resources. CORS also relies on a mechanism by which
+              browsers make a "preflight" request to the server hosting the
+              cross-origin resource, in order to check that the server will
+              permit the actual request. In that preflight, the browser sends
+              headers that indicate the HTTP method and headers that will be
+              used in the actual request.
+            </p>
+          </div>
         </div>
+        <div
+          tabIndex={0}
+          className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            Why are you using firebase? What other options do you have to
+            implement authentication?
+          </div>
+          <div className="collapse-content">
+            <p>
+              I am using firebase for authentication like login sign Up etc.
+              <br />
+              The options i have for authentication given below.
+              <br /> Back4App <br /> Backendless <br /> Kuzzle <br /> Pubnub{" "}
+              <br /> Kumulos <br />
+              Appwrite <br /> Deployd <br /> NHost <br /> AWS Amplify <br />{" "}
+              Heroku.
+            </p>
+          </div>
+        </div>
+        <div
+          tabIndex={0}
+          className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            How does the private route work?
+          </div>
+          <div className="collapse-content">
+            <p>
+              The react private route component renders child components
+              (children) if the user is logged in. If not logged in the user is
+              redirected to the /login page with the return url passed in the
+              location state property.
+            </p>
+          </div>
+        </div>
+        <div
+          tabIndex={0}
+          className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+        >
+          <div className="collapse-title text-xl font-medium">
+            What is Node? How does Node work?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Node.js is an open source, cross-platform runtime environment for
+              developing server-side and networking applications. Node.js
+              applications are written in JavaScript, and can be run within the
+              Node.js runtime on OS X, Microsoft Windows, and Linux. Node.js
+              also provides a rich library of various JavaScript modules which
+              simplifies the development of web applications using Node.js to a
+              great extent. <br />
+              It is a used as backend service where javascript works on the
+              server-side of the application. This way javascript is used on
+              both frontend and backend. Node.js runs on chrome v8 engine which
+              converts javascript code into machine code, it is highly scalable,
+              lightweight, fast, and data-intensive.
+            </p>
+          </div>
+        </div>
+      </div>
     );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,49 @@
 import React from 'react';
 import homeimg from '../../assects/homeimg.png'
 import Header from '../Header/Header';
+import ReactToPdf from "react-to-pdf";
+
+
 
 const Home = () => {
+  const ref = React.createRef();
+  const options = {
+    orientation: "landscape",
+    unit: "in",
+    format: [4, 2],
+  };
     return (
       <div>
         <Header></Header>
         <div className="flex items-center mt-3 justify-center flex-col sm:flex-col md:flex-row">
           <div className=" w-[90%] md:w-[50%]">
             <h1 className="text-5xl text-left py-5">
-              Learn Effectively that will take you towards your goal
+              Your dream to become a full stack developer is on step away!
             </h1>
-            <button
-              type="button"
-              className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
-            >
-              Download course documentation
-            </button>
+            <p className='py-3'>Passion + Dedication + Hard Word = Success!</p>
+            <div>
+              <ReactToPdf
+                targetRef={ref}
+                filename="div-blue.pdf"
+                options={options}
+                x={0.5}
+                y={0.5}
+                scale={0.8}
+              >
+                {({ toPdf }) => (
+                  <button
+                    className="text-green-700 block hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
+                    onClick={toPdf}
+                  >
+                    Download course documentation
+                  </button>
+                )}
+              </ReactToPdf>
+              <div
+                style={{ width: 10, height: 10,  }}
+                ref={ref}
+              ></div>
+            </div>
           </div>
           <div className="">
             <img src={homeimg} alt="" />
